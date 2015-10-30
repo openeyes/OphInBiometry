486626da4a94e719bde4f1386cd0a78c060d11cc
himanshupatel79 <contacttohimanshu@yahoo.com>
2015-10-30 15:41:47 +0000

    OE-5759 :: Disabled IOL Power & Predicted Refraction for Auto Event.

--- views/default/form_Element_OphInBiometry_Selection_fields.php
+++ views/default/form_Element_OphInBiometry_Selection_fields.php
@@ -38,7 +38,7 @@
 		</div>
 	</div>
 	<div id="div_Element_OphInBiometry_Selection_<?php echo $side?>" >
-		<?php echo $form->textField($element, 'iol_power_'.$side, null, null, array('label'=>4, 'field'=>2))?>
-		<?php echo $form->textField($element, 'predicted_refraction_'.$side, null, null, array('label'=>4, 'field'=>2))?>
+		<?php echo $form->textField($element, 'iol_power_'.$side,  ($this->is_auto) ? array('readonly'=>true) : null , null, array('label'=>4, 'field'=>2))?>
+		<?php echo $form->textField($element, 'predicted_refraction_'.$side, ($this->is_auto) ? array('readonly'=>true) : null, null, array('label'=>4, 'field'=>2))?>
 	</div>
 </div>
