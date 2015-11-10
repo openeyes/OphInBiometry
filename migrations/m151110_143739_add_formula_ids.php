708314f082ce4492c4065335e5f9a6ceb7b47b36
himanshupatel79 <contacttohimanshu@yahoo.com>
2015-11-10 15:40:08 +0000

    OE-5831 ::  Added Migration

--- migrations/m151110_143739_add_formula_ids.php
+++ migrations/m151110_143739_add_formula_ids.php
@@ -12,7 +12,7 @@ class m151110_143739_add_formula_ids extends CDbMigration
 	public function down()
 	{
 		//Drop Column
-		$this->dropColumn('et_ophinbiometry_measurement', 'formula_id_left');
-		$this->dropColumn('et_ophinbiometry_measurement', 'formula_id_right');
+		$this->dropColumn('et_ophinbiometry_selection', 'formula_id_left');
+		$this->dropColumn('et_ophinbiometry_selection', 'formula_id_right');
 	}
 }
