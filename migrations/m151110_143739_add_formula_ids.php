142a9515ade9306249bc74cc4b3038f2c447684e
himanshupatel79 <contacttohimanshu@yahoo.com>
2015-11-10 16:08:26 +0000

    OE-5831 ::  Updated Migration

--- migrations/m151110_143739_add_formula_ids.php
+++ migrations/m151110_143739_add_formula_ids.php
@@ -1,12 +1,12 @@
 <?php
 
-class m151110_143739_add_formula_ids extends CDbMigration
+class m151110_143739_add_formula_ids extends OEMigration
 {
 	public function up()
 	{
 		//Add Column
-		$this->addColumn('et_ophinbiometry_selection', 'formula_id_left', 'int(10) unsigned NOT NULL DEFAULT 1');
-		$this->addColumn('et_ophinbiometry_selection', 'formula_id_right', 'int(10) unsigned NOT NULL DEFAULT 1');
+		$this->addColumn('et_ophinbiometry_selection', 'formula_id_left', 'int(10) unsigned NOT NULL DEFAULT 0');
+		$this->addColumn('et_ophinbiometry_selection', 'formula_id_right', 'int(10) unsigned NOT NULL DEFAULT 0');
 	}
 
 	public function down()
