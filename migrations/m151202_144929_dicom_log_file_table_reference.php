b51a2f21aaa49abbeece0437349423c98da9fcd2
vetusko <vetusko@gmail.com>
2015-12-02 15:25:08 +0000

    migration fix

--- migrations/m151202_144929_dicom_log_file_table_reference.php
+++ migrations/m151202_144929_dicom_log_file_table_reference.php
@@ -7,12 +7,12 @@ class m151202_144929_dicom_log_file_table_reference extends CDbMigration
 		$this->dropColumn('dicom_import_log','file_name');
 		$this->dropColumn('dicom_import_log','file_path');
 		$this->addColumn('dicom_import_log', 'dicom_file_id', 'int(10) unsigned');
-		$this->addForeignKey('dicom_file_id_ref','dicom_import_log','dicom_file_id', 'dicom_file','id');
+		$this->addForeignKey('dicom_import_file_id_ref','dicom_import_log','dicom_file_id', 'dicom_files','id');
 	}
 
 	public function down()
 	{
-		$this->dropForeignKey('dicom_file_id_ref', 'dicom_import_log');
+		$this->dropForeignKey('dicom_import_file_id_ref', 'dicom_import_log');
 		$this->dropColumn('dicom_import_log','dicom_file_id');
 		$this->addColumn('dicom_import_log', 'file_name', 'varchar(255)');
 		$this->addColumn('dicom_import_log', 'file_path', 'varchar(400)');
