8ba62341976ccf9aff6d83b1d86ad22167649934
himanshupatel79 <contacttohimanshu@yahoo.com>
2015-12-14 16:36:44 +0000

    OE-5831:: the selected lens is still not highlighted

--- views/default/view_Element_OphInBiometry_Selection_fields.php
+++ views/default/view_Element_OphInBiometry_Selection_fields.php
@@ -18,11 +18,11 @@
         <div class="row data-row">
             <div class="large-3 column">
                 <div
-                    class="data-label"><?php echo CHtml::encode($element->getAttributeLabel('lens_id_' . $side)) ?>
+                    class="data-label "><?php echo CHtml::encode($element->getAttributeLabel('lens_id_' . $side)) ?>
                 </div>
             </div>
             <div class="large-9 column end">
-                <div class="data-value"
+                <div class="data-value iolDisplay"
                      id="lens_<?php echo $side ?>"><?php echo $element->{'lens_' . $side} ? $element->{'lens_' . $side}->name : 'None' ?></div>
             </div>
         </div>
