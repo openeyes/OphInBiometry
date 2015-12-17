f198a29fb2bc2d3be1024bececa0e1d9d65959a6
vetusko <vetusko@gmail.com>
2015-12-17 14:04:14 +0000

    migration fix

--- migrations/m151216_135138_add_content_time_surgeon.php
+++ migrations/m151216_135138_add_content_time_surgeon.php
@@ -5,7 +5,7 @@ class m151216_135138_add_content_time_surgeon extends CDbMigration
 	public function up()
 	{
 		$this->addColumn('ophinbiometry_imported_events','content_datetime', 'datetime after is_merged');
-		$this->addColumn('ophinbiometry_imported_events','surgeon_name', 'varchar(255) after content_time');
+		$this->addColumn('ophinbiometry_imported_events','surgeon_name', 'varchar(255) after content_datetime');
 	}
 
 	public function down()
