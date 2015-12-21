8c2c9eb90e935ac873d2acfff02ff5fab58b65a3
himanshupatel79 <contacttohimanshu@yahoo.com>
2015-12-21 16:43:49 +0000

    OE-5865 , OE-5885  :: updated last column

--- migrations/m151221_122711_add_additional_measurements.php
+++ migrations/m151221_122711_add_additional_measurements.php
@@ -9,10 +9,10 @@ class m151221_122711_add_additional_measurements extends CDbMigration
 		$this->addColumn('et_ophinbiometry_measurement','k_modified_left', 'boolean default false');
 		$this->addColumn('et_ophinbiometry_measurement','k_modified_right', 'boolean default false');
 
-		$this->addColumn('et_ophinbiometry_measurement_version','al_modified_left', 'boolean default false after  comments');
-		$this->addColumn('et_ophinbiometry_measurement_version','al_modified_right', 'boolean default false after comments');
-		$this->addColumn('et_ophinbiometry_measurement_version','k_modified_left', 'boolean default false after comments');
-		$this->addColumn('et_ophinbiometry_measurement_version','k_modified_right', 'boolean default false after comments');
+		$this->addColumn('et_ophinbiometry_measurement_version','al_modified_left', 'boolean default false after  eye_status_right');
+		$this->addColumn('et_ophinbiometry_measurement_version','al_modified_right', 'boolean default false after eye_status_right');
+		$this->addColumn('et_ophinbiometry_measurement_version','k_modified_left', 'boolean default false after eye_status_right');
+		$this->addColumn('et_ophinbiometry_measurement_version','k_modified_right', 'boolean default false after eye_status_right');
 	}
 
 	public function down()
