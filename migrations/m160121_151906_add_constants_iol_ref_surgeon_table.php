9814166523d5c071b994a3d3ebdd07b4acbb8bd4
vetusko <vetusko@gmail.com>
2016-01-21 17:11:47 +0000

    added columns to the _version table

--- migrations/m160121_151906_add_constants_iol_ref_surgeon_table.php
+++ migrations/m160121_151906_add_constants_iol_ref_surgeon_table.php
@@ -14,6 +14,13 @@ class m160121_151906_add_constants_iol_ref_surgeon_table extends CDbMigration
 		$this->addColumn('et_ophinbiometry_iol_ref_values', 'active', 'boolean default true');
 		$this->addColumn('et_ophinbiometry_iol_ref_values', 'surgeon_id', 'int(11)');
 		$this->addForeignKey('surgeon_fk','et_ophinbiometry_iol_ref_values', 'surgeon_id', 'ophinbiometry_surgeon', 'id');
+
+		$this->addColumn('et_ophinbiometry_iol_ref_values_version', 'constant', 'decimal(6,2)');
+		$this->addColumn('et_ophinbiometry_iol_ref_values_version', 'constant1', 'decimal(6,2)');
+		$this->addColumn('et_ophinbiometry_iol_ref_values_version', 'constant2', 'decimal(6,2)');
+		$this->addColumn('et_ophinbiometry_iol_ref_values_version', 'active', 'boolean default true');
+		$this->addColumn('et_ophinbiometry_iol_ref_values_version', 'surgeon_id', 'int(11)');
+
 	}
 
 	public function down()
@@ -24,6 +31,12 @@ class m160121_151906_add_constants_iol_ref_surgeon_table extends CDbMigration
 		$this->dropColumn('et_ophinbiometry_iol_ref_values', 'active');
 		$this->dropForeignKey( 'surgeon_fk', 'et_ophinbiometry_iol_ref_values');
 		$this->dropColumn('et_ophinbiometry_iol_ref_values', 'surgeon_id');
+
+		$this->dropColumn('et_ophinbiometry_iol_ref_values_version', 'constant');
+		$this->dropColumn('et_ophinbiometry_iol_ref_values_version', 'constant1');
+		$this->dropColumn('et_ophinbiometry_iol_ref_values_version', 'constant2');
+		$this->dropColumn('et_ophinbiometry_iol_ref_values_version', 'active');
+		$this->dropColumn('et_ophinbiometry_iol_ref_values_version', 'surgeon_id');
 
 		$this->dropTable('ophinbiometry_surgeon');
 	}
