3117989fe4c623d971fcd6bb159817086e5d0cf3
himanshupatel79 <contacttohimanshu@yahoo.com>
2016-01-25 14:48:18 +0000

    OE-5919 :: Edit mode changes added

--- assets/js/module.js
+++ assets/js/module.js
@@ -287,18 +287,22 @@ function updateIolRefTable(side) {
 	if (side == 'left') {
 		$('table[id^="left_"]').hide();
 		$('span[id^="emmetropia_left_"]').hide();
+		$('span[id^="aconstant_left_"]').hide();
 	}
 
 	if (side == 'right') {
 		$('table[id^="right_"]').hide();
 		$('span[id^="emmetropia_right_"]').hide();
+		$('span[id^="aconstant_right_"]').hide();
 	}
 
 	if (!isNaN(parseInt(l_id)) && !isNaN(parseInt(f_id))) {
 		var swtb = side + '_' + l_id + '_' + f_id;
 		var swsn = 'emmetropia_'+side + '_' + l_id + '_' + f_id;
+		var asn = 'aconstant_'+side + '_' + l_id + '_' + f_id;
 		$('table[id^=' + swtb + ']').show();
 		$('span[id^=' + swsn + ']').show();
+		$('span[id^=' + asn + ']').show();
 	}
 }
 
