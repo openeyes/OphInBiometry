62d8fa6f27b07a2e90f2537a4040a12a6bad2474
himanshupatel79 <contacttohimanshu@yahoo.com>
2016-01-25 16:21:03 +0000

    OE-5919 ::  Added  Surgon changes and Fixed issue of exception on  deleted event

--- views/default/view.php
+++ views/default/view.php
@@ -27,14 +27,15 @@ $this->beginContent('//patient/event_container');
 $this->moduleNameCssClass.=" highlight-fields";
 
 if($this->is_auto) {
-	$imported_event_data = OphInBiometry_Imported_Events::model()->findByAttributes(array('event_id' => $this->event->id));
 ?>
 <div id="surgeon" class="row data-row">
 	<div class="large-2 column" style="margin-left: 10px;">
 		<div class="data-label">Surgeon:</div>
 	</div>
 	<div class="large-9 column end">
-		<div class="data-value"><b><?php echo ($imported_event_data->surgeon_name); ?></b></div>
+		<div class="data-value"><b><?php echo (OphInBiometry_Surgeon::model()->findByAttributes(
+					array('id' => Element_OphInBiometry_IolRefValues::model()->findByAttributes(array('event_id' => $this->event->id))->surgeon_id)
+				)->name); ?></b></div>
 	</div>
 </div>
 <?php
